3ec2bfdf2b54d5414ff58f4cb3461b9b2b76a5ba
Enzo Fernandes <enzofalcantara@gmail.com>
2024-11-08 10:32:43 -0300

    update atual

--- index.js
+++
@@ -1,9 +0,0 @@
-
-let links = document.getElementsByClassName('menu');
-
-for (let i = 0; i < links.length; i++) {
-    links[i].addEventListener('click', () => {
-        links[i].classList.toggle('active')
-        console.log(i)
-    })   
-}
